6ec2010f7e6f2a31f606db7b9e39b6b0f1f0ae91
Amritpal Singh <aamritpa@sfu.ca>
2020-08-01 23:19:05 -0700

    JS file added

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,4 @@
+
 /* Linear Search Algorithm */
 function linear_search(array,target){
     if(array.length <=0){
@@ -8,14 +9,48 @@ function linear_search(array,target){
     {
         if(array[i]==target)
         {
-          window.print("Element found"); 
+           console.log("Element found at ", i); 
           return 0; 
         }
     }
-    window.print("Element  not found");
+    alert("Element  not found");
     return -1; 
 }
 
 /* Binary Search Algorithm  */
 
+function binary_search(array,target){
 
+    var mid = array.length / 2;
+    var first = 0;
+    var last = array.length -1;
+    if (mid <= 0)
+    {
+        console.log("Element not found"); 
+        return -1;
+    }
+
+    while (last!=first+1)
+    {
+        if (array[mid] == target) 
+        {
+            console.log("Element foundat ", mid); 
+            return 0;
+        }
+        else if (array[mid] > target)
+        {
+            last = mid;
+            mid = (last + first) / 2;
+        }
+        else if (array[mid] < target)
+        {
+            first = mid;
+            mid = (last + first) / 2;
+        }
+    }
+    console.log("Element not found"); 
+    return -1;
+}
+
+var array2 = [1,3,5,7,9,13,15,26,56,89];
+var search = binary_search(array2,26,10);
